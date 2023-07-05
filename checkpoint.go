a4932d180bf3fefa66d626605722a539ddcf9a6f
sanathsshetty <120259170+sanathsshetty@users.noreply.github.com>
2023-07-05 15:33:38 +0530

    Create checkpoint.go

---
+++ checkpoint.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, checkpoint chan bool, resume chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Worker %d Starting\n", id)
+	time.Sleep(time.Duration(id) * time.Second)
+	fmt.Printf("Worker %d checkpoint reached\n", id)
+	checkpoint <- true
+	<-resume
+	fmt.Printf("Worker %d Resuming\n", id)
+}
+
+func main() {
+	numWorkers := 5
+	checkpoint := make(chan bool)
+	resume := make(chan bool)
+	var wg sync.WaitGroup
+
+	for i := 1; i <= numWorkers; i++ {
+		wg.Add(1)
+		go worker(i, checkpoint, resume, &wg)
+	}
+
+	for i := 1; i <= numWorkers; i++ {
+		<-checkpoint
+	}
+	fmt.Println("All workers reached the checkpoint")
+	fmt.Println("Resuming all workers")
+
+	for i := 1; i <= numWorkers; i++ {
+		resume <- true
+	}
+	wg.Wait()
+	fmt.Println("All workers completed their work")
+}
